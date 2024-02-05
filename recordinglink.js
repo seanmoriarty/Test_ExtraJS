b8aaa1f1d0a012ce86f62c6fee33ee5e67a891c3
Sean Moriarty <smoriarty81@gmail.com>
2024-02-05 17:49:10 -0500

    Update recordinglink.js

--- recordinglink.js
+++ recordinglink.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   Array.from(table.rows).forEach(function(row) {
     var button = document.createElement("a");
     button.textContent = "Copy Link";
-    button.setAttribute('class', 'save');
+    button.setAttribute('class', 'save.helpsy');
     button.setAttribute('title', 'Copy Link');
     button.onclick = function() {
       copyToClipboard(this);
