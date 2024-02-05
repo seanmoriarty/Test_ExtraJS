0b20c9518da30e47cbcf4bb8df893d0e7a6cbc2c
Sean Moriarty <smoriarty81@gmail.com>
2024-02-05 12:15:28 -0500

    Update recordinglink.js

--- recordinglink.js
+++ recordinglink.js
@@ -1,4 +1,4 @@
-nddocument.addEventListener("DOMContentLoaded", function() {
+document.addEventListener.("DOMContentLoaded", function() {
   // Select the table by its ID
   var table = document.getElementById("call-history-table");
 
