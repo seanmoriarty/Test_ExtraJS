f2536808c2091ce84e1ec65859ab2545124af312
Sean Moriarty <smoriarty81@gmail.com>
2024-02-05 14:36:55 -0500

    Update recordinglink.js

--- recordinglink.js
+++ recordinglink.js
@@ -1,11 +1,7 @@
-// JavaScript to insert buttons and modify the copyToClipboard function
 document.addEventListener("DOMContentLoaded", function() {
-  // Select the table by its ID
   var table = document.getElementById("call-history-table");
 
-  // Iterate over each row in the table
   Array.from(table.rows).forEach(function(row) {
-    // Create a new button element
     var button = document.createElement("button");
     button.textContent = "Copy Audio";
     button.setAttribute('class', 'save');
@@ -13,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
       copyToClipboard(this);
     };
 
-    // Find the cell with the class 'action-buttons' and append the button
     var actionCell = row.querySelector(".action-buttons");
     if (actionCell) {
       actionCell.appendChild(button);
@@ -22,10 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function copyToClipboard(btnElement) {
-  // Get the link with the specific class in the same row as the button
   var link = btnElement.closest("tr").querySelector(".download-audio.helpsy").href;
 
-  // Copy the link to the clipboard
   navigator.clipboard.writeText(link).then(function() {
     console.log('Link copied to clipboard!');
     alert("Link Copied");
