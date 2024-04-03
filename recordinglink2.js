77f6d193a9997fce577a6f68bf4e320835e2ee85
Sean Moriarty <smoriarty81@gmail.com>
2024-04-03 11:05:23 -0400

    Update recordinglink2.js

--- recordinglink2.js
+++ recordinglink2.js
@@ -17,65 +17,13 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-class Toast {
-  constructor(message,color,time){
-    this.message = message;
-    this.color = color;
-    this.time = time;
-    this.element = null;
-    var element = document.createElement('div');
-    element.className = "toast-notification";
-    this.element = element;
-    var countElements = document.getElementsByClassName("toast-notification");
-    
-    element.style.opacity=0.8;
-    
-    element.style.marginBottom = (countElements.length * 55) + "px";
-    
-    element.style.backgroundColor = this.color;
-    
-    var message = document.createElement("div");
-    message.className = "message-container";
-    message.textContent = this.message;
-    
-    element.appendChild(message);
-    
-    var close = document.createElement("div");
-    close.className = "close-notification";
-    
-    var icon = document.createElement("i");
-    icon.className = "lni lni-close";
-    
-    close.appendChild(icon);
-    
-    element.append(close);
-    
-    document.body.appendChild(element);
-    
-    setTimeout(function() {
-      element.remove();
-    }, this.time);
-    
-    close.addEventListener("click",()=>{
-      element.remove();
-    })
-  }
-  
-}
-
-const ToastType = {
-  Danger : "#eb3b5a",
-  Warning: "#fdcb6e",
-  Succes : "#00b894",
-}
-
 function copyToClipboard(btnElement) {
   var link = btnElement.closest("tr").querySelector(".download-audio.helpsy").href;
 
   navigator.clipboard.writeText(link).then(function() {
     console.log('Link copied to clipboard!');
-    alert(link + "\n Copied!");
-    new Toast(link + "Copied!",ToastType.Succes,5000);
+    alert("Copied Link: " + link);
+
   }).catch(function(error) {
     console.error('Error copying link: ', error);
     alert("Error Copying");
