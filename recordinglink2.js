bbe8682f37a52ced52c29c48bbfb595043c829ff
Sean Moriarty <smoriarty81@gmail.com>
2024-04-03 12:25:33 -0400

    Update recordinglink2.js

--- recordinglink2.js
+++ recordinglink2.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   Array.from(table.rows).forEach(function(row) {
     var button = document.createElement("a");
-    button.textContent = "Copy Link";
+    button.textContent = "Copy Link" + row.querySelector(".download-audio.helpsy").href;
     button.setAttribute('class', 'save');
     button.setAttribute('title', 'Copy Link');
     button.onclick = function() {
